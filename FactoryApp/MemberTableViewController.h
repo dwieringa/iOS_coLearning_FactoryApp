62b1125f5b1cf795e53bf9cebe9e91ceddf72354
Dave Wieringa <dwjm@userwise.com>
2014-06-10 12:04:44 -0400

    Connect Member Table with Parse.com

---
+++ FactoryApp/MemberTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MemberTableViewController.h
+//  FactoryApp
+//
+//  Created by David Wieringa on 6/10/14.
+//  Copyright (c) 2014 Userwise Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MemberTableViewController : UITableViewController
+
+@end
