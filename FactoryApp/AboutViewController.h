70b719112477e54d52d0879ed0e2d0a4583b19b9
Dave Wieringa <dwjm@userwise.com>
2014-06-23 20:15:01 -0400

    Improve fonts on About and Detail page. Wire up buttons on About page

---
+++ FactoryApp/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  FactoryApp
+//
+//  Created by David Wieringa on 6/23/14.
+//  Copyright (c) 2014 Userwise Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
---
+++ FactoryApp/AboutViewController.m
@@ -0,0 +1,64 @@
+//
+//  AboutViewController.m
+//  FactoryApp
+//
+//  Created by David Wieringa on 6/23/14.
+//  Copyright (c) 2014 Userwise Solutions. All rights reserved.
+//
+
+#import "AboutViewController.h"
+
+@interface AboutViewController ()
+
+@end
+
+@implementation AboutViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)twitterButtonPressed:(UIButton *)sender {
+}
+
+- (IBAction)facebookButtonPressed:(id)sender {
+}
+
+- (IBAction)emailUsPressed:(id)sender {
+}
+
+- (IBAction)callUsPressed:(id)sender {
+}
+
+- (IBAction)visitUsPressed:(id)sender {
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
