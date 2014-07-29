c33df4a22a354b755eab2952d42367fad61b0564
Dave Wieringa <dwjm@userwise.com>
2014-07-29 11:31:49 -0400

    Rework Space page to use pan gesture vs swipe so that images can move with gesture vs after

--- FactoryApp/SpaceViewController.m
+++ FactoryApp/SpaceViewController.m
@@ -9,7 +9,16 @@
 #import "SpaceViewController.h"
 #import "SWRevealViewController.h"
 
-@interface SpaceViewController ()
+static NSInteger const RequiredLengthOfSwipeGesture = 100;
+
+@interface SpaceViewController () {
+    NSInteger imageViewWidth;
+    NSInteger imageViewHeight;
+    NSInteger imageViewY;
+    NSInteger mainImageViewX;
+    NSInteger leftImageViewX;
+    NSInteger rightImageViewX;
+}
 @property (weak, nonatomic) IBOutlet UIImageView *mainImageView;
 @property (weak, nonatomic) IBOutlet UILabel *descriptionLabel;
 @property (weak, nonatomic) IBOutlet UIImageView *leftImageView;
@@ -17,6 +26,8 @@
 @property (strong, nonatomic, readonly) NSArray *photos;
 @property (strong, nonatomic, readonly) NSArray *descriptions;
 @property (nonatomic) NSInteger selectedImageIndex;
+@property (nonatomic, readonly) NSInteger leftImageIndex;
+@property (nonatomic, readonly) NSInteger rightImageIndex;
 
 @end
 
@@ -38,7 +49,6 @@
                              [UIImage imageNamed:@"kitchen-clear"],
                              [UIImage imageNamed:@"meeting room-clear"],
                              [UIImage imageNamed:@"open space-clear"],
-                             //[UIImage imageNamed:@"open space-blurred"],
                              [UIImage imageNamed:@"open space 2-clear"],
                              [UIImage imageNamed:@"skype room-clear"],
                              nil];
@@ -52,7 +62,6 @@
                                          [NSString stringWithFormat:@"Kitchen"],
                                          [NSString stringWithFormat:@"Meeting Room"],
                                          [NSString stringWithFormat:@"Open Space"],
-                                         //[NSString stringWithFormat:@"open space-blurred"],
                                          [NSString stringWithFormat:@"Open Space 2"],
                                          [NSString stringWithFormat:@"Skype Room"],
                                          nil];
@@ -64,22 +73,23 @@
     _selectedImageIndex = imageIndex;
     if (_selectedImageIndex < 0) _selectedImageIndex = self.photos.count-1;
     if (_selectedImageIndex >= self.photos.count) _selectedImageIndex = 0;
-    
-    self.mainImageView.image = self.photos[_selectedImageIndex];
-    self.descriptionLabel.text = [self.descriptions[_selectedImageIndex] uppercaseString];
+}
 
-    // load left image view
+- (NSInteger)leftImageIndex
+{
     if (_selectedImageIndex == 0) {
-        self.leftImageView.image = self.photos[self.photos.count-1];
+        return self.photos.count-1;
     } else {
-        self.leftImageView.image = self.photos[_selectedImageIndex-1];
+        return _selectedImageIndex-1;
     }
-    
-    // load right image view
+}
+
+- (NSInteger)rightImageIndex
+{
     if (_selectedImageIndex == self.photos.count-1) {
-        self.rightImageView.image = self.photos[0];
+        return 0;
     } else {
-        self.rightImageView.image = self.photos[_selectedImageIndex+1];
+        return _selectedImageIndex+1;
     }
 }
 
@@ -87,16 +97,22 @@
 {
     [super viewDidLoad];
     
+    self.selectedImageIndex = 0;
+    
+    self.mainImageView.image = self.photos[self.selectedImageIndex];
     self.mainImageView.layer.borderColor = [UIColor whiteColor].CGColor;
     self.mainImageView.layer.borderWidth = 1.5;
-    
+    self.mainImageView.frame = CGRectMake(mainImageViewX, imageViewY, imageViewWidth, imageViewHeight);
+
+    self.leftImageView.image = self.photos[self.leftImageIndex];
     self.leftImageView.layer.borderColor = [UIColor whiteColor].CGColor;
     self.leftImageView.layer.borderWidth = 1.5;
+    self.leftImageView.frame = CGRectMake(leftImageViewX, imageViewY, imageViewWidth, imageViewHeight);
     
+    self.rightImageView.image = self.photos[self.rightImageIndex];
     self.rightImageView.layer.borderColor = [UIColor whiteColor].CGColor;
     self.rightImageView.layer.borderWidth = 1.5;
-    
-    self.selectedImageIndex = 0;
+    self.rightImageView.frame = CGRectMake(rightImageViewX, imageViewY, imageViewWidth, imageViewHeight);
     
     // Change button color
     _menuButton.tintColor = [UIColor lightGrayColor];
@@ -106,8 +122,19 @@
     _menuButton.action = @selector(revealToggle:);
     
     // Set the gesture
-    [self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
+    //[self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
 
+}
+
+- (void)viewDidLayoutSubviews
+{
+    // init image location/size values based on autolayouts outcome
+    imageViewWidth = self.mainImageView.frame.size.width;
+    imageViewHeight = self.mainImageView.frame.size.height;
+    imageViewY = self.mainImageView.frame.origin.y;
+    mainImageViewX = self.mainImageView.frame.origin.x;
+    leftImageViewX = self.leftImageView.frame.origin.x;
+    rightImageViewX = self.rightImageView.frame.origin.x;
 }
 
 - (void)didReceiveMemoryWarning
@@ -116,14 +143,85 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)leftSwipeDetected:(UISwipeGestureRecognizer *)sender
-{
-    self.selectedImageIndex = self.selectedImageIndex + 1;
-}
+- (IBAction)pan:(UIPanGestureRecognizer *)recognizer {
+    bool swipeCompleted = NO;
+    
+    if (recognizer.state == UIGestureRecognizerStateChanged ||
+        recognizer.state == UIGestureRecognizerStateEnded) {
+        
+        // move images horizontally with gesture
+        NSInteger xTranslation = [recognizer translationInView:self.view].x;
+        self.mainImageView.frame = CGRectMake(mainImageViewX+xTranslation, imageViewY, imageViewWidth, imageViewHeight);
+        self.leftImageView.frame = CGRectMake(leftImageViewX+xTranslation, imageViewY, imageViewWidth, imageViewHeight);
+        self.rightImageView.frame = CGRectMake(rightImageViewX+xTranslation, imageViewY, imageViewWidth, imageViewHeight);
+        
+        // determine if swipe action should be completed
+        if (abs(xTranslation) > RequiredLengthOfSwipeGesture) {
 
-- (IBAction)rightSwipeDetected:(UISwipeGestureRecognizer *)sender
-{
-    self.selectedImageIndex = self.selectedImageIndex - 1;
+            // stop the gesture since it is now compmlete
+            recognizer.enabled = NO;
+            recognizer.enabled = YES;
+            swipeCompleted = YES;
+            
+            if (xTranslation > 0) {
+                // detected swipe to the right
+                
+                // right image view is wrapping around to become the left now (with new image)
+                UIImageView *newLeftImageView = self.rightImageView;
+                newLeftImageView.frame = CGRectMake((leftImageViewX-(mainImageViewX-leftImageViewX))+xTranslation, imageViewY, imageViewWidth, imageViewHeight);
+                self.rightImageView = self.mainImageView;
+                self.mainImageView = self.leftImageView;
+                self.leftImageView = newLeftImageView;
+                
+                // load left image view
+                self.selectedImageIndex -= 1;
+                newLeftImageView.image = self.photos[self.leftImageIndex];
+                
+            } else if (xTranslation < 0) {
+                // detected swipe to the left
+                
+                // left image view is wrapping around to become the right now (with new image)
+                UIImageView *newRightImageView = self.leftImageView;
+                newRightImageView.frame = CGRectMake(rightImageViewX+(rightImageViewX-mainImageViewX)+xTranslation, imageViewY, imageViewWidth, imageViewHeight);
+                self.leftImageView = self.mainImageView;
+                self.mainImageView = self.rightImageView;
+                self.rightImageView = newRightImageView;
+                
+                // load right image view
+                self.selectedImageIndex += 1;
+                newRightImageView.image = self.photos[self.rightImageIndex];
+            }
+        }
+        
+        if (recognizer.state == UIGestureRecognizerStateEnded || swipeCompleted) {
+            // animate the 3 images into their proper positions
+            [UIView animateWithDuration:.25
+                                  delay:0
+                                options:UIViewAnimationOptionCurveLinear
+                             animations:^ {
+                                 self.leftImageView.frame = CGRectMake(leftImageViewX, imageViewY, imageViewWidth, imageViewHeight);
+                                 self.mainImageView.frame = CGRectMake(mainImageViewX, imageViewY, imageViewWidth, imageViewHeight);
+                                 self.rightImageView.frame = CGRectMake(rightImageViewX, imageViewY, imageViewWidth, imageViewHeight);
+                             }
+                             completion:^(BOOL finished) {}];
+            
+            // fade old description label out
+            [UIView animateWithDuration:.25
+                                  delay:0
+                                options:UIViewAnimationOptionCurveLinear
+                             animations:^ { self.descriptionLabel.alpha = 0; }
+                             completion:^(BOOL finished) {
+                                 self.descriptionLabel.text = self.descriptions[self.selectedImageIndex];
+                             }];
+            // fade new description label in
+            [UIView animateWithDuration:.25
+                                  delay:.25
+                                options:UIViewAnimationOptionCurveLinear
+                             animations:^ { self.descriptionLabel.alpha = 1; }
+                             completion:^(BOOL finished) {}];
+        }
+        
+    }
 }
 
 /*
